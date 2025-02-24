8536df552a389822d3c4980402392c8b4a187da3
nate <natejohnson0592@gmail.com>
2025-02-23 18:29:57 -0600

    json adjust

--- pages/api/schools.js
+++ pages/api/schools.js
@@ -1,6 +1,6 @@
 export default async function handler(req, res) {
   try {
-    const data = require("../..//public/data/schools.json");
+    const data = require("../../public/data/schools.json");
     res.status(200).json(data);
   } catch (error) {
     console.error("Error fetching school data:", error);
